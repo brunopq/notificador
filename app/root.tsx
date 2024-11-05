2ce6a56664a0d7f20434106a7589baff5526f0d4
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2024-11-05 11:38:39 -0300

    first commit for real now

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="min-h-screen text-stone-200 bg-stone-900">
         {children}
         <ScrollRestoration />
         <Scripts />
